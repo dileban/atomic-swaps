722dbaef460740a9b5f81bcad92cda4cbf69831a
Dileban Karunamoorthy <dileban@gmail.com>
2018-11-28 11:40:01 +1100

    Add configuration for Rinkeby

--- ethereum/truffle.js
+++ ethereum/truffle.js
@@ -1,9 +1,19 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        },
+        rinkebyInfura: {
+            provider: function() {
+            },
+            network_id: "4"          
+        },       
+        rinkebyLocal: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "4"
+        }
     }
-  }
 };
